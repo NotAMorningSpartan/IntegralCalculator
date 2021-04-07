54be6a689f386f0c2a089f7a46e1f5ad3f9006b0
Tyler Kness-Miller <tylerkm75@gmail.com>
2021-04-07 06:15:17 -0600

    Added input of all parameters except function in main(). Added TODO comments.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 //Tyler Kness-Miller
 //HW7
+//13 April 2021
 
 using namespace std;
 #include <iostream>
 
-double trap_int(double a, double b, int n, function f){
+//TODO: Modify functions to accept function as parameter.
+double trap_int(double a, double b, int n){
 
 }
 
-double simp_int(double a, double b, int n, function f){
+double simp_int(double a, double b, int n){
 
 }
 
 int main(){
+    double a;
+    double b;
+    int n;
+
+    cout << "Please input the start of the range you wish to integrate on:" << endl;
+    cin >> a;
+    cout << "Please input the end of the range you wish to integrate on:" << endl;
+    cin >> b;
+    cout << "Please input the number of sections in the range of the integration:" << endl;
+    cin >> n;
+    cout << "Please input the function you wish to be numerically integrated:" << endl;
+    //TODO: Implement passing in a function.
+
+    cout << "Result using the trapezoid method: " << to_string(trap_int(a, b, n)) << endl;
+    cout << "Result using Simpson's method of integration: " << to_string(simp_int(a, b, n)) << endl;
     return 0;
 }
