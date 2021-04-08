ec742cea1376a463d6863388c4dff04a2b523ed8
Tyler Kness-Miller <tylerkm75@gmail.com>
2021-04-08 12:05:06 -0600

    Fixed bug in trapezoid equation. Added comments and more print statements to improve usability.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 //HW7
 //13 April 2021
 
+//Articles that assisted me:
+//https://www.codesansar.com/numerical-methods/integration-simpson-1-3-method-algorithm.htm
+//http://theflyingkeyboard.net/algorithms/trapezoidal-rule-algorithm/
+
+
 //Include statements.
 using namespace std;
 #include <iostream>
@@ -22,15 +27,16 @@ double f(double x){
 /// \return The calculated value from the integration.
 double trap_int(double a, double b, int n, function<double(double x)> f){
     //Grid spacing/segment size
-    double h = (b-a) / n;
+    double h = (b - a) / n;
+
     double s = f(a) + f(b);
     for(int i = 0; i < n; i++){
-        s += 2 * f(a + i + h);
+        s += 2 * f(a + i * h);
     }
-    return (h/2) * s;
+    return (h / 2) * s;
 }
 
-/// Method that integrates a given function over a given range using Simpson's method.
+/// Method that integrates a given function over a given range using Simpson's 1/3 rule.
 /// \param a The start of the range.
 /// \param b The end of the range.
 /// \param n The number of sections.
@@ -38,6 +44,7 @@ double trap_int(double a, double b, int n, function<double(double x)> f){
 /// \return The calculated value from the integration.
 double simp_int(double a, double b, int n, function<double(double x)> f){
     double h = (b - a) / n;
+    //Store values.
     double x[n+1], y[n+1];
     for(int i = 0; i <= n; i++){
         x[i] = a + i * h;
@@ -62,7 +69,9 @@ double simp_int(double a, double b, int n, function<double(double x)> f){
     return result;
 }
 
+//Main function.
 int main(){
+    //Define inputs.
     double a;
     double b;
     int n;
@@ -73,7 +82,7 @@ int main(){
     cin >> b;
     cout << "Please input the number of sections in the range of the integration:" << endl;
     cin >> n;
-
+    cout << "Calculating value of integration using equation x^2 + 1..." << endl;
     cout << "Result using the trapezoid method: " << to_string(trap_int(a, b, n, f)) << endl;
     cout << "Result using Simpson's method of integration: " << to_string(simp_int(a, b, n, f)) << endl;
     return 0;
